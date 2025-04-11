1917c30f15aeba41aa2caf11e431cc3ea6d9d709
Chantel Gee <chmagee@ucsc.edu>
2025-04-10 19:49:11 -0700

    js validated

--- labs/lab6/javascript/lab6.js
+++ labs/lab6/javascript/lab6.js
@@ -4,21 +4,34 @@
 // Date: 04.10.25
 //
 
-document.addEventListener("DOMContentLoaded", init)
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
+  const music = document.querySelector(".music");
   const vid = document.getElementById("vidLinkMain");
   const aud = document.getElementById("audLinkMain");
   const videoPlayer = document.getElementById("videoDiv");
   const audioPlayer = document.getElementById("audioDiv");
 
+  /**
+     * prepare the audio and video for playing
+     */
+    music.src = "audio/backroomLabyrinth.mp3";
+    music.load();
+    music.volume = 1;
+
   // switch between video and audio
   vid.addEventListener("click", (e) => {
-    videoPlayer.classList.remove("hidden")
+    videoPlayer.classList.remove("hidden");
     audioPlayer.classList.add("hidden");
+    music.pause();
+    music.currentTime = 0;
   });
   aud.addEventListener("click", (e) => {
-    videoPlayer.classList.add("hidden")
+    videoPlayer.classList.add("hidden");
     audioPlayer.classList.remove("hidden");
   });
+
+  
+
 } // end init function
